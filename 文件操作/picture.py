6d03a68cba07b0a201663bf31aac5372ee2f487b
“745184483@qq.com” <745184483@qq.com>
2023-08-18 16:50:43 +0800

    Changes

--- 文件操作/picture.py
+++ 文件操作/picture.py
@@ -160,47 +160,123 @@ def change_background_color(image_path, new_color):
 
 import cv2
 import numpy as np
+
+
 def picture():
     # 读取照片
-    image=cv2.imread(img_file)
-    #修改尺寸
-    image = cv2.resize(image,None,fx=0.5,fy=0.5)
+    image = cv2.imread(img_file)
+    # 修改尺寸
+    image = cv2.resize(image, None, fx=0.5, fy=0.5)
     # 图片转换为二值化图
-    hsv = cv2.cvtColor(image,cv2.COLOR_BGR2HSV)
-    #print(hsv)
+    hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+    # print(hsv)
     # 图片的二值化黑白处理
-    lower_red = np.array([110,70,150])
-    upper_red = np.array([150,255,255])
+    lower_red = np.array([110, 70, 150])
+    upper_red = np.array([150, 255, 255])
     heibai = cv2.inRange(hsv, lower_red, upper_red)
 
-    #闭运算
-    k=np.ones((5,5),np.uint8)
-    r=cv2.morphologyEx(heibai,cv2.MORPH_CLOSE,k)
-    #原图显示
-    cv2.imshow('image',image)
-    #颜色替换
-    rows,cols,channels = image.shape
+    # 闭运算
+    k = np.ones((5, 5), np.uint8)
+    r = cv2.morphologyEx(heibai, cv2.MORPH_CLOSE, k)
+    # 原图显示
+    cv2.imshow('image', image)
+    # 颜色替换
+    rows, cols, channels = image.shape
     for i in range(rows):
-      for j in range(cols):
-        if r[i,j]==255: # 像素点为255表示的是白色，我们就是要将白色处的像素点，替换为红色
-          image[i,j]=(219,142,67) # 此处替换颜色，为BGR通道，不是RGB通道
-    #新图显示
-    cv2.imshow('red',image)
-    #无限等待
+        for j in range(cols):
+            if r[i, j] == 255:  # 像素点为255表示的是白色，我们就是要将白色处的像素点，替换为红色
+                image[i, j] = (219, 142, 67)  # 此处替换颜色，为BGR通道，不是RGB通道
+    # 新图显示
+    cv2.imshow('red', image)
+    # 无限等待
     cv2.waitKey(0)
-    #销毁内存
+    # 销毁内存
     cv2.destroyAllWindows()
-    #新图保存
-    cv2.imwrite('H:\\pictures\\redzhengjian.jpg',image)
+    # 新图保存
+    cv2.imwrite('H:\\pictures\\redzhengjian.jpg', image)
 
 
+import os
+import argparse
 
+import numpy as np
+import cv2 as cv
+
+
+# 对证件照进行三种颜色的换底操作
+def change_color(image_path, output_path):
+    # 读入数据
+    image = cv.imread(image_path)
+
+    h, w, ch = image.shape
+
+    data = image.reshape((-1, 3))
+    # 数据转换，计算更快
+    data = np.float32(data)
+
+    # 设置聚类
+    criteria = (cv.TERM_CRITERIA_EPS + cv.TERM_CRITERIA_MAX_ITER, 10, 1.0)
+    num_clusters = 4
+    # label就是各个像素点的所属的类别标签，通常第一个像素点的标签就是背景色的标签
+    _, label, _ = cv.kmeans(data, num_clusters, None, criteria, num_clusters, cv.KMEANS_RANDOM_CENTERS)
+
+    # 找到背景像素的类别
+    indx = label[0][0]
+
+    # 生成掩膜
+    mask = np.ones((h, w), dtype=np.uint8) * 255
+    label = np.reshape(label, (h, w))
+
+    mask[label == indx] = 0
+
+    # 处理掩膜，使得生成的图像更加完美
+    se = cv.getStructuringElement(cv.MORPH_RECT, (3, 3))
+    cv.erode(mask, se, mask)
+    mask = cv.GaussianBlur(mask, (5, 5), 0)
+
+    # 白色背景[255, 255, 255]
+    bg_w = np.ones(image.shape, dtype=np.float) * 255
+
+    # 红色背景 注意：opencv中的像素三通道的排列顺序为BGR，而非RGB。所以红色为[0, 0, 255]， 蓝色为[255, 0, 0]
+    red = np.array([0, 0, 255])
+    bg_r = np.tile(red, (h, w, 1))
+
+    # 蓝色背景
+    blue = np.array([255, 0, 0])
+    bg_b = np.tile(blue, (h, w, 1))
+
+    # alpha 即为需要保留的部分，则 1-alpha 为要去除的背景，此处可以看作两张图像的加权融合
+    alpha = mask.astype(np.float32) / 255
+    fg = alpha[..., None] * image
+    bg = 1 - alpha[..., None]
+    new_image_white = fg + bg * bg_w
+    new_image_red = fg + bg * bg_r
+    new_image_blue = fg + bg * bg_b
+
+    # 保存图片
+    w_path = os.path.join(output_path, 'white.jpg')
+    r_path = os.path.join(output_path, 'red.jpg')
+    b_path = os.path.join(output_path, 'blue.jpg')
+
+    cv.imwrite(w_path, new_image_white.astype(np.uint8))
+    cv.imwrite(r_path, new_image_red.astype(np.uint8))
+    cv.imwrite(b_path, new_image_blue.astype(np.uint8))
+
+    print('OK done!')
 
 
 if __name__ == '__main__':
     # read_text_in_picture()
     # repair_the_photos()
     # photos_change_background_color()
-    #change_background_color(img_newfile,(0, 0, 255,255))
-    picture()
+    # change_background_color(img_newfile,(0, 0, 255,255))
+    # picture()
+    '''
+    # ap = argparse.ArgumentParser()
+    # ap.add_argument('-i', '--image', default='./image.jpg', help='path to the input image.')
+    # ap.add_argument('-p', '--path', default='.', help='path to the output image.')
+    # args = vars(ap.parse_args())
+    '''
+
+    change_color(img_file, 'H:\\pictures\\')
     pass
